87f15e2dc4f43cec74326b6f109839d3b054fb4a
eber28 <eberromanpumachahua@gmail.com>
2024-11-15 09:41:46 -0500

    landing en proceso 2

--- PELICULA_LANDING/PELICULA_LANDING.py
+++ PELICULA_LANDING/PELICULA_LANDING.py
@@ -1,38 +1,61 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
 
-from rxconfig import config
-
-
-class State(rx.State):
-    """The app      state."""
-
-    ...
-
-
 def index() -> rx.Component:
-    # Welcome Page (Index)
     return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+
+        rx.heading("PELICULAS-YA",
+        color="red",
+        size="6",
+        padding="20px",
+        hight="100vh",
+        width="100%",
+        position="absolute",
+        top="0",
+        left="0"
+        ),
+        rx.link(
+             rx.button("iniciar secion",bg="red",href="https://forms.gle/rptSMjFmtEQqKEMW9"),position="absolute",
+        top="20px",
+        right="20px",
+        z_index=10
+        ),
+        rx.box(
+             rx.button("registrate   ",bg="red"),position="absolute",
+        top="20px",
+        right="150px",
+        z_index=10
+        ),
+        rx.hstack(
             rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
+                "PELICULAS-YA ",
+                size="5", color="white"
+            ),
+            rx.text(
+                "Food Puquio es una aplicación móvil diseñada para conectar a los amantes de la comida con una amplia gama de restaurantes y opciones culinarias. Su nombre, Food Puquio, evoca la imagen de un manantial de sabores, haciendo referencia a la abundancia y variedad de opciones que ofrece la aplicación",
                 size="5",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
+            rx.vstack(
+                rx.hstack(
+                    rx.link(
+                        rx.button("Registrate aqui!",color_scheme="green"),
+                        href="https://forms.gle/Gj2CstmbZuhy1V2c9",
+                        is_external=True,
+                    ),
+                    
+                ),
+                
             ),
             spacing="5",
             justify="center",
             min_height="85vh",
         ),
-        rx.logo(),
-    )
+
+        height="100vh",
+        width="100%",
+        background="url('https://www.teknofilo.com/wp-content/uploads/2021/06/Netflix-1280x720.jpg')",
+        background_size="cover",
+        background_position="center"
+    ),
 
 
 app = rx.App()
---
+++ README.md
@@ -0,0 +1,50 @@
+# NOMBRE DE APP
+   *PELICULAS-YA*
+# PROBLEMATICA
+el costo elevado de  las app que se encargan de proyectar peliculas del año o franquicias 
+del cine mundial  como podrian ser NETFLIX, DISNEY, OTROS
+
+# SOLUCION
+LA solucion crear una APP en el cual cada pelicula tendria un costo minimo y de buena calidad
+ya no pagarian una "mensualidad" o una "suscripcion" sino pagarian solo de las peliculas que desean
+vizualizar 
+# CASOS DE USO
+los casos de uso que va realizar el usuario en mi APP
+![alt text](image.png)
+
+# HERRAMIENTAS QUE UTILIZARE
+las herramientas que boya utilizar para poder crear mi mvp y tambien para poder hacer la documentacion 
+y programar mis tareas son los siguientes 
+
+## FORMULARIO DE GOOGLE
+utilizare el google forms  para poder realizar mi mvp 
+en forma de cuestionario 
+
+![alt text](image-1.png)
+
+## HERRAMMIENTAS PARA DIFUNDIR MI MVP
+Las herramienmtas que elegi para compartir mi mvp 
+fue *whatsapp* ya que la mayoria de personas lo utilizan
+
+![alt text](image-3.png)
+## HERRAMIENTA PARA ORGINZAR MIS TAREAS 
+la pagina wed *TRELLO* fue mi eleccion ya que las tareas lo puedes ordenar de manera facil 
+y sin complicasiones 
+
+https://trello.com/invite/b/672cd700f3f7df8942d9678d/ATTI4c83f69ecd910126aba0e3a12072153d3927DB38/apppeliculas
+
+![alt text](image-5.png)
+# (PRODUCTO MINIMO VIABLE) MVP
+Para mi MVP ey utilizado las herramientas de google mas especifico 
+el (google forms)
+
+ enlase de mi mvp   https://forms.gle/yqBYKckxK8geStPo8
+
+# MEDICION DEL MVP
+el formulario tiene un grafico circular el cual filtra las respuestas 
+de las personas que respondieron
+
+![alt text](image-6.png)
+![alt text](image-7.png)
+# PESTAÑA DE INICIO (LANDING PAGE)
+
